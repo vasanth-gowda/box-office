129484f83e03b8adc5d80a87c61a2626c0973119
vasanth <vasanthgowda74@gmail.com>
2022-10-07 21:16:16 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
-  return <div>LALA</div>;
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route to="/">
+          <Home />
+        </Route>
+        <Route to="/">
+          <Starred />
+        </Route>
+        <div>
+          <Route>404 Not found</Route>
+        </div>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
